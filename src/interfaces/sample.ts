9c90df49a510f3deb82f9e92f168dc621d998db6
Seoyoung Jo <sy.jo@break.co.kr>
2021-06-15 14:52:50 +0900

    init: setting

---
+++ src/interfaces/sample.ts
@@ -0,0 +1,19 @@
+export interface price {
+    psa: string | number;
+    brg: string | number;
+    break: string | number;
+};
+
+export interface CardType {
+    // playerImage?: string;
+    id: number;
+    smallTitle: string;
+    title: string;
+    tag: string;
+    minimum: price;
+    maximum?: price;
+    recently: price;
+    // minimum: Array<price>;
+    // maximum?: Array<price>;
+    // recently: Array<price>;
+};
