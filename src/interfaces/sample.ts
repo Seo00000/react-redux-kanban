df6f1fa11e05b7cb31f707f30562778c062eaa4c
Seoyoung Jo <josy2665@gmail.com>
2021-06-15 16:47:44 +0900

    fix: function name 추가, scss 수정

--- src/interfaces/sample.ts
+++ src/interfaces/sample.ts
@@ -1,19 +1,19 @@
 export interface price {
-    psa: string | number;
-    brg: string | number;
-    break: string | number;
-};
+  psa: string | number;
+  brg: string | number;
+  break: string | number;
+}
 
 export interface CardType {
-    // playerImage?: string;
-    id: number;
-    smallTitle: string;
-    title: string;
-    tag: string;
-    minimum: price;
-    maximum?: price;
-    recently: price;
-    // minimum: Array<price>;
-    // maximum?: Array<price>;
-    // recently: Array<price>;
-};
+  // playerImage?: string;
+  id: number;
+  smallTitle: string;
+  title: string;
+  tag: string;
+  minimum: price;
+  maximum?: price;
+  recently: price;
+  // minimum: Array<price>;
+  // maximum?: Array<price>;
+  // recently: Array<price>;
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
-export default function () {
+export default function test() {
   return null;
 }
